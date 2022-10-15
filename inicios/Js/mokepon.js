fe99aeab953090c751b543e0da9bcbcabc6ce6e2
shezitse <aioncron@gmail.com>
2022-10-15 05:40:54 -0400

    comienzo html

---
+++ inicios/Js/mokepon.js
@@ -0,0 +1,159 @@
+let ataqueJugador
+let ataqueEnemigo
+let vidasJugador= 3
+let vidasEnemigo= 3
+
+function iniciarJuego()
+{
+    let botonMascotaJugador = document.getElementById( "boton-mascota" )
+    botonMascotaJugador.addEventListener( "click", seleccionarMascotaJugador )
+
+    let botonFuego = document.getElementById( "boton-fuego" )
+    botonFuego.addEventListener("click", ataqueFuego)
+    let botonAgua = document.getElementById( "boton-agua" )
+    botonAgua.addEventListener("click", ataqueAgua)
+    let botonTierra = document.getElementById( "boton-tierra" )
+    botonTierra.addEventListener("click", ataqueTierra)
+}
+
+function seleccionarMascotaJugador()
+{
+    let inputHipodoge = document.getElementById( "hipodoge" )
+    let inputCapipepo = document.getElementById( "capipepo" )
+    let inputRatigueya = document.getElementById( "ratigueya" )
+    let spanMascotaJugador = document.getElementById( "mascota-jugador" )
+
+    if ( inputHipodoge.checked )
+    {
+        spanMascotaJugador.innerHTML = "hipodoge"
+    } else if ( inputCapipepo.checked )
+    {
+        spanMascotaJugador.innerHTML = "capipepo"
+    } else if ( inputRatigueya.checked )
+    {
+        spanMascotaJugador.innerHTML = "ratigueya"
+    } else
+    {
+        alert( "selecciona una mascota" )
+    }
+
+    seleccionarMascotaEnemigo()
+}
+function seleccionarMascotaEnemigo()
+{
+    let mascotaAleatoria = aleatorio( 1, 3 )
+    let spanMascotaEnemigo = document.getElementById( "mascota-enemigo" )
+    
+    if ( mascotaAleatoria == 1 )
+    {
+     spanMascotaEnemigo.innerHTML= "Hipodoge"
+    } else if ( mascotaAleatoria == 2 )
+    {
+        spanMascotaEnemigo.innerHTML= "Capipepo"
+    } else
+    {
+        spanMascotaEnemigo.innerHTML= "Ratigueya"
+ }
+}
+
+function ataqueFuego()
+{
+    ataqueJugador = "FUEGO"
+ataqueAleatorioEnemigo()
+}
+
+function ataqueAgua()
+{
+  ataqueJugador = "AGUA"
+ataqueAleatorioEnemigo()  
+}
+
+function ataqueTierra()
+{
+ ataqueJugador = "TIERRA"
+ataqueAleatorioEnemigo()    
+}
+
+function ataqueAleatorioEnemigo()
+{
+   let ataqueAleatorio=aleatorio( 1, 3) 
+    
+    if ( ataqueAleatorio == 1 )
+    {
+        ataqueEnemigo="FUEGO"
+    } else if ( ataqueAleatorio == 2 )
+    {
+        ataqueEnemigo="AGUA"
+    } else
+    {
+        ataqueEnemigo="TIERRA"
+    }
+
+    combate()
+}
+
+function combate()
+{
+    let spanVidasJugador = document.getElementById( "vidas-jugador" )
+    let spanVidasEnemigo=document.getElementById("vidas-enemigo")
+    if (ataqueEnemigo == ataqueJugador) {
+        crearMensaje("EMPATE")
+    } else if (ataqueJugador == "FUEGO" && ataqueEnemigo == "TIERRA") {
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML= vidasEnemigo
+    } else if (ataqueJugador == "AGUA" && ataqueEnemigo == "FUEGO") {
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML= vidasEnemigo
+    } else if (ataqueJugador == "TIERRA" && ataqueEnemigo == "AGUA") {
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML= vidasEnemigo
+    } else {
+        crearMensaje("PERDISTE")
+        vidasJugador--
+        spanVidasJugador.innerHTML= vidasJugador
+    }
+
+    revisarVidas()
+}
+
+function revisarVidas()
+{
+    if ( vidasEnemigo == 0 )
+    {
+        crearMensajeFinal("FELICITACIONES")
+    } else if ( vidasJugador==0)
+    {
+        //perdiste
+
+    }
+}
+
+function crearMensaje(resultado)
+{
+    let sectionMensajes = document.getElementById("mensajes")
+    
+    let parrafo = document.createElement( "p" )
+    parrafo.innerHTML = "Tu mascota ataco con " + ataqueJugador + ", la mascota del enemigo ataco con " + ataqueEnemigo + "- " + resultado
+
+    sectionMensajes.appendChild(parrafo)
+}
+
+unction crearMensajeFinal(resultadoFinal)
+{
+    let sectionMensajesFinal = document.getElementById("mensajes")
+    
+    let parrafo = document.createElement( "p" )
+    parrafo.innerHTML = resultadoFinal
+
+    sectionMensajes.appendChild(parrafo)
+}
+
+
+function aleatorio( min, max )
+{ 
+return Math.floor( Math.random() * ( max - min + 1 ) + min )
+    }
+    window.addEventListener( "load", iniciarJuego )
