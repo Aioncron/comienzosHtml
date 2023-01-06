740f9aecabc4e3fff1a4b752c82b3812f093edf1
shezitse <aioncron@gmail.com>
2023-01-05 22:29:19 -0400

    Estilos Mquepon

--- inicios/Js/mokepon.js
+++ inicios/Js/mokepon.js
@@ -55,7 +55,7 @@ function seleccionarMascotaEnemigo()
     sectionSeleccionarMascota.style.display = "none"
     
     let sectionSeleccionarAtaque = document.getElementById( "seleccionar-ataque" )
-    sectionSeleccionarAtaque.style.display = "block"
+    sectionSeleccionarAtaque.style.display = "flex"
     
     let mascotaAleatoria = aleatorio( 1, 3 )
     let spanMascotaEnemigo = document.getElementById( "mascota-enemigo" )
